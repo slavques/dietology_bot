29936d6402aba417f47eec66cd1b5e560e00d6c8
Vyacheslav Chekunov <78318333+slavques@users.noreply.github.com>
2025-06-14 18:21:09 +0300

    Refactor bot into modular package

---
+++ bot/handlers/start.py
@@ -0,0 +1,17 @@
+from aiogram import types, Dispatcher
+
+from ..database import SessionLocal, User
+
+async def cmd_start(message: types.Message):
+    session = SessionLocal()
+    user = session.query(User).filter_by(telegram_id=message.from_user.id).first()
+    if not user:
+        user = User(telegram_id=message.from_user.id)
+        session.add(user)
+        session.commit()
+    session.close()
+    await message.reply("Привет! Отправь фото блюда, и я рассчитаю КБЖУ.")
+
+
+def register(dp: Dispatcher):
+    dp.register_message_handler(cmd_start, commands=['start'])
